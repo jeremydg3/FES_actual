bae9aa5679d2df4ef76e55bde6f40e42100da286
Jeremy Warren <jeremydg3@gmail.com>
2020-02-29 22:52:33 -0600

    serial connectivity works and changed layout

--- GUI/FES_main.py
+++ GUI/FES_main.py
@@ -2,52 +2,83 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.properties import ListProperty
+from kivy.properties import BooleanProperty
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy_garden.graph import MeshLinePlot
 from kivy.graphics import *
+from kivy.uix.spinner import Spinner
 import serial
 from serial import Serial
 import sys
 
 connected = False
-recording = False 
-redC = (1,0,0)
-greenC = (0,1,0)
-# This function comes graciously from StackOverflow
-# Finds all available serial ports, and returns as a list
-def FindAllSerialPorts():
-	if sys.platform.startswith('win'):
-		ports = ['COM%s' % (i + 1) for i in range(256)]
-	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-		# this excludes your current terminal "/dev/tty"
-		ports = glob.glob('/dev/tty[A-Za-z]*')
-	elif sys.platform.startswith('darwin'):
-		ports = glob.glob('/dev/tty.*')
-	else:
-		raise EnvironmentError('Unsupported platform')
+recording = False
+GREEN = [0,1,0,0.5]
+RED   = [1,0,0,0.5]
 
-	result = []
-	print("Refreshing USB COM ports.")
-	for port in ports:
-		try:
-			s = serial.Serial(port)
-			s.close()
-			result.append(port)
-			print("...Good",end="")
-		except:
-			print("...",end="")
-		print('')
-	if len(result) == 0:
-		result.append('NONE')
-	
-	return result
+serialPort = 'NONE'
 
 
 # Window Classes**********************************************
 class serialWindow(Screen):
-    print('serial')
-    
+    result = ListProperty(['Select Port'])
+    serialIndicatorC = ListProperty(RED)
+    recordIndicatorC = ListProperty(RED)
+
+    # This function comes graciously from StackOverflow
+    # Finds all available serial ports, and returns as a list
+    def FindAllSerialPorts(self):
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+        self.result.clear()
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                self.result.append(port)
+            except:
+                t = 0
+        if len(self.result) == 0:
+            self.result.append('NONE')
+
+    def setPort(self, serialport):
+        global serialPort
+        print(serialport)
+        serialPort = serialport
+
+    # connect to the specified serial port
+    def connectToSerial(self):
+        global connected
+        baudrate = 115200
+        try:
+            self.ser = serial.Serial(serialPort, baudrate, timeout = 0.1)
+            self.ser.flushInput()
+        except:
+            self.serialIndicatorC = RED
+            connected = False
+            print('Error connecting')
+            return -1
+        print('Connection successful')
+        connected = True
+        self.serialIndicatorC = GREEN
+        return
+
+    def disconnectFromSerial(self):
+        global connected
+        if connected == True:
+            self.ser.close()
+            connected = False
+            self.serialIndicatorC = RED
+        print('disconnected')
 
 
 class calibrationWindow(Screen):
@@ -55,6 +86,17 @@ class calibrationWindow(Screen):
     lhval = ObjectProperty(None)
     rtval = ObjectProperty(None)
     rhval = ObjectProperty(None)
+    serialIndicatorC = ListProperty(RED)
+    recordIndicatorC = ListProperty(RED)
+    
+    def updateIndicators(self):
+        print('update')
+        self.serialIndicatorC = ListProperty(RED)
+        self.recordIndicatorC = ListProperty(RED)
+        if connected:
+            self.serialIndicatorC = ListProperty(GREEN)
+        if recording:
+            self.recordIndicatorC = ListProperty(GREEN)
 
     def startFSRCalibration(self):
         print('starting calibration')
@@ -68,10 +110,28 @@ class calibrationWindow(Screen):
 
 
 class debugWindow(Screen):
+    if connected:
+        serialIndicatorC = ListProperty(GREEN)
+    else:
+        serialIndicatorC = ListProperty(RED)
+
+    if recording:
+        recordIndicatorC = ListProperty(GREEN)
+    else:
+        recordIndicatorC = ListProperty(RED)
     print('debug')
 
 
 class fesWindow(Screen):
+    if connected:
+        serialIndicatorC = ListProperty(GREEN)
+    else:
+        serialIndicatorC = ListProperty(RED)
+
+    if recording:
+        recordIndicatorC = ListProperty(GREEN)
+    else:
+        recordIndicatorC = ListProperty(RED)
     print('fes')
 
 
@@ -98,6 +158,7 @@ sm.current = 'serial'
 class mainApp(App):
     def build(self):
         return sm
+            
 
 # Run main app
 if __name__ == '__main__':
